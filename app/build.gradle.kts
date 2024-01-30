cec0de5b10fa2a625d1b0539283baf3aa2923ee4
Juan Nascimento <juannascimento048@gmail.com>
2024-01-30 14:49:48 -0300

    chore: update dependencies to the latest versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,13 +45,13 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
